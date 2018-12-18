ee740ba0cc04b275ea38dbb19dcb257e8c85aaf9
JSannake <JSannake@GP-PIN-IS51.printograph.pvt>
2018-12-18 14:25:29 +0530

    jaydeep.sannake : Sample Code Push

---
+++ TestSpringSample/src/main/java/com/example/demo/TestSpringSampleApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class TestSpringSampleApplication {
+
+	public static void main(String args[]) {
+		
+		SpringApplication.run(TestSpringSampleApplication.class, args);
+		
+	}
+}
+
